b78df7db96330d86ea5b9fbb276ddb7e89f02972
Rian <rian.hsu@bxb.tw>
2024-09-10 14:20:41 +0800

    change url

--- app.ts
+++ app.ts
@@ -17,7 +17,7 @@ const init = async () => {
   // Once you deploy a MinIO service in the same project with this app,
   // Zeabur will automatically set these environment variables for you.
 
-  let endPoint = process.env.MINIO_CONSOLE_URL
+  let endPoint = process.env.MINIO_INTERNAL_URL
   if (!endPoint) {
     console.info('MINIO_CONSOLE_URL is not set. Did you deploy a MinIO service?')
     console.info('If you are running this app locally, you can get the endpoint from the "domain" tab of MinIO service in the Zeabur dashboard.')
